ae21dc31dfe46983e9b19f070899d4ed2f2ea6e5
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-10-28 18:15:57 +0300

    feat: add emits

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    @click="$emit('click')"
     :disabled="disabled"
     class="inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
     :class="{ 'opacity-50': disabled }"
@@ -28,6 +29,10 @@ export default {
       requared: false,
       default: false,
     },
+  },
+
+  emits: {
+    click: null,
   },
 };
 </script>
